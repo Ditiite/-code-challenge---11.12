63e419fc94a727734cd7e3d5c6611281f778cf90
Dita <Ditiite@users.noreply.github.com>
2017-12-14 11:51:56 +0200

    Fixed title colors,. Text and image card title with time and heart. Fixed read more / less botton.

--- Front Page/index.js
+++ Front Page/index.js
@@ -1,10 +1,15 @@
 // Display text when press link - read more
 function showText() {
+    let read = document.getElementById('read-less');
     let text = document.getElementById('more-text');
+    
     if(text.style.display === 'none') {
         text.style.display = 'block';
+        read.innerHTML = "/ Read Less /";
     } else {
         text.style.display = 'none';
+        read.innerHTML = "/ Read More /";
     }
+   
 }
 
