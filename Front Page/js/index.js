6609b80e7556523902e90416686b8a30a78011cb
Dita <Ditiite@users.noreply.github.com>
2017-12-14 17:19:00 +0200

    Falling snowflackes

--- Front Page/js/index.js
+++ Front Page/js/index.js
@@ -1,15 +1,19 @@
-// Display text when press link - read more
+// Display text when press link - read more and change read more <--> read less
 function showText() {
     let read = document.getElementById('read-less');
     let text = document.getElementById('more-text');
-    
-    if(text.style.display === 'none') {
+
+    if(text.style.display === 'none' || text.style.display === '') {
         text.style.display = 'block';
         read.innerHTML = "/ Read Less /";
     } else {
         text.style.display = 'none';
         read.innerHTML = "/ Read More /";
-    }
-   
+    }   
 }
 
+
+
+$( document ).ready( function() {
+    $( 'body' ).flurry();
+});
